1537c5d9c1c213d88ee664975f72221cc4098b56
Michael Hutton <michaelhutton@gmail.com>
2013-09-05 17:32:43 +1000

    New carousel (without iPad pic)

--- js/shiftly.js
+++ js/shiftly.js
@@ -1,1 +1,11 @@
+$('#myCarousel .carousel-text').hide();
+$('#myCarousel .active .carousel-text').show();
 $('.carousel').carousel();
+
+$('#myCarousel').on('slide',function(){
+    $('#myCarousel .active .carousel-text').fadeOut(300);
+});
+
+$('#myCarousel').on('slid',function(){
+    $('#myCarousel .active .carousel-text').fadeIn();
+});
