460d037f2c8b5d9b6f7cf708e8c05b8f92f2b0fe
Michael Hutton <michaelhutton@gmail.com>
2014-04-30 01:29:10 +1000

    Add "signup" event for Google Analytics.

--- js/shiftly.js
+++ js/shiftly.js
@@ -10,3 +10,14 @@ $('#myCarousel').on('slid',function(){
     $('#myCarousel .active .carousel-text').fadeIn(300);
     $('#myCarousel .active .carousel-image').fadeIn(1000);
 });
+
+$('a.signup').on('click',function(e){
+	e.preventDefault();
+	ga('send', 'event', {
+		'eventCategory' : 'signup',
+		'eventAction'   : 'click',
+			console.log("asdf");
+			window.location.href = e.target.href;
+		}
+	});
+});
